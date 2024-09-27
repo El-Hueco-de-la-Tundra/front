d8aa256cfc4b557363c3c98fc817e1137ccbbd07
mat-hayes <mateo.hayes@mi.unc.edu.ar>
2024-09-27 17:25:11 -0300

    design changes

--- src/components/buttonspage.jsx
+++ src/components/buttonspage.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const ButtonsPage = ({ onCreateGame, onListGames }) => {
   return (
     <div className="buttons-container">
-      <button className="custom-button" onClick={onCreateGame}>Crear partida</button>
-      <button className="custom-button" onClick={onListGames}>Unirse a partida</button>
+      <button className="custom-button" onClick={onCreateGame}>Crear Partida</button>
+      <button className="custom-button" onClick={onListGames}>Unirse a Partida</button>
     </div>
   );
 };
